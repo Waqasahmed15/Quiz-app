27fb5eb95a4633caa1806531919da9cca8316e0d
Waqas Ahmad <waqasahmedwikki15@gmail.com>
2024-03-29 18:41:41 +0500

    html and css added

---
+++ app.js
@@ -0,0 +1,38 @@
+const quizdData = [
+  {
+    questions: "What does HTML Stand For?",
+    options: [
+      "Hyper Text Markup Language",
+      "Hyper Transfer Markup Language",
+      "Hyper Machine Markup Language",
+      "Hyper and Text Markup Language",
+    ],
+    correct: 0,
+  },
+  {
+    questions:
+      "Whic CSS Property is used  to control the spacing between elements?",
+    options: ["margin", "padding", "spacing", "border-spacing"],
+    correct: 0,
+  },
+  {
+    questions: "What is the Javascript ?",
+    options: [
+      "Hyper Text Markup Language",
+      "Hyper Transfer Markup Language",
+      "Hyper Machine Markup Language",
+      "Hyper and Text Markup Language",
+    ],
+    correct: 0,
+  },
+  {
+    questions: "What does HTML Stand For?",
+    options: [
+      "Hyper Text Markup Language",
+      "Hyper Transfer Markup Language",
+      "Hyper Machine Markup Language",
+      "Hyper and Text Markup Language",
+    ],
+    correct: 0,
+  },
+];

7fdf8b5a0d2b27a1494a7d7d1288ba9c8cd4ecd4
Waqas Ahmad <waqasahmedwikki15@gmail.com>
2024-03-31 16:52:57 +0500

    second step

--- app.js
+++ app.js
@@ -13,26 +13,47 @@ const quizdData = [
     questions:
       "Whic CSS Property is used  to control the spacing between elements?",
     options: ["margin", "padding", "spacing", "border-spacing"],
+    correct: 1,
+  },
+  {
+    questions: "What is the Javascript function used to select HTML by its id?",
+    options: [
+      "document.query",
+      "getElementById",
+      "selectElemnet",
+      "findElementById",
+    ],
+    correct: 1,
+  },
+  {
+    questions:
+      "In React.Js which hook is used to preform side effects in a function componant?",
+    options: ["useEffect", "useState", "useContext", "useReducer"],
     correct: 0,
   },
   {
-    questions: "What is the Javascript ?",
-    options: [
-      "Hyper Text Markup Language",
-      "Hyper Transfer Markup Language",
-      "Hyper Machine Markup Language",
-      "Hyper and Text Markup Language",
-    ],
-    correct: 0,
-  },
-  {
-    questions: "What does HTML Stand For?",
-    options: [
-      "Hyper Text Markup Language",
-      "Hyper Transfer Markup Language",
-      "Hyper Machine Markup Language",
-      "Hyper and Text Markup Language",
-    ],
-    correct: 0,
+    questions: "which HTML tag is used to create an order list?",
+    options: ["<ul>", "<li>", "<ol>", "<dl>"],
+    correct: 2,
   },
 ];
+// java script initialization;
+const answerElm = document.querySelectorAll(".answer");
+const [questionElm, option_1, option_2, option_3, option_4] =
+  document.querySelectorAll(
+    "#question, .option_1, .option_2 .option_3 .option_4 "
+  );
+const submitbtn = document.querySelector("#submit");
+const currentQuiz = 0;
+const score = 0;
+
+//load QuizFunction
+const loadQuiz = () => {
+  const { questions, options } = quizdData[currentQuiz];
+  questionElm.innerText = questions;
+  options.forEach(
+    (curOption, index) => (window[`option_${index + 1}`].innerText = curOption)
+  );
+};
+
+loadQuiz();
